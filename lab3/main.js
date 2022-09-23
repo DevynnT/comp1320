9772e9ee79371213384642016ea5fe2b2d169a29
Devynn <personaldevynn@gmail.com>
2022-09-23 15:40:07 -0700

    Finish mathHelpers and

--- lab3/main.js
+++ lab3/main.js
@@ -1,9 +1,40 @@
 const process = require('process');
 const fs = require('fs');
+const mathHelpers = require('./mathHelpers.js');
 
-const args = process.argv;
+const directory = "./dataPoints";
+const formattedArgs = process.argv.slice(2); // remove the first 2 args as they are uneeded
 
-const processInput = () => {
-    fs.mkdir()
-    fs.writeFile("points.txt", )
+const processInput = (args) => {
+    const point1 = `${args[0]},${args[1]}`;
+    const point2 = `${args[2]},${args[3]}`;
+    const calculatedDistance = mathHelpers.distance(parseInt(args[0]), parseInt(args[2]), parseInt(args[1]), parseInt(args[3])); // the elements inside args are strings so convert to integer
+
+    console.log(`The distance between your two points: (${point1}), (${point2}) is ${calculatedDistance}`);
+
+    fs.mkdir(directory, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            fs.writeFile(directory + "./points.txt", "utf-8", (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Content saved")
+                }
+
+                fs.appendFile(directory + "./points.txt", data, (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                })
+            })
+        }
+    })
 };
+
+if (formattedArgs.length == 4) {
+    processInput(formattedArgs);
+} else {
+    console.log("You did provided too many or not enough arguments.")
+}
--- lab3/mathHelpers.js
+++ lab3/mathHelpers.js
@@ -1,11 +1,11 @@
-const squareRoot = () => {
+const squareRoot = (num) => Math.sqrt(num);
 
+const square = (num) => num * num;
+
+const distance = (numX1, numY1, numX2, numY2) => {
+    const squaredResult = square(numX2 - numX1) + square(numY2 - numY1);
+
+    return squareRoot(squaredResult);
 };
 
-const square = () => {
-
-};
-
-const distance = () => {
-    
-};
+module.exports = {distance};
