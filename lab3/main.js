4d2470eb6046ca2be3a2301a31b508488db41773
Devynn <personaldevynn@gmail.com>
2022-09-26 13:45:13 -0700

    Finish main.js with recursion

--- lab3/main.js
+++ lab3/main.js
@@ -2,39 +2,52 @@ const process = require('process');
 const fs = require('fs');
 const mathHelpers = require('./mathHelpers.js');
 
-const directory = "./dataPoints";
+const dirName = "dataPoints";
 const formattedArgs = process.argv.slice(2); // remove the first 2 args as they are uneeded
 
-const processInput = (args) => {
+const checkDir = (dirName) => {
+    let returnedDirName = dirName;
+
+    if (fs.existsSync(dirName)) {
+        const newDirName = "temp_" + dirName;
+
+        console.log(`Folder ${dirName} already exists. Creating a new folder for you called ${newDirName}`);
+
+        returnedDirName = checkDir(newDirName);
+
+        return returnedDirName;
+    } else {
+        return returnedDirName;
+    }
+};
+
+const processInput = (args, dirName) => {
     const point1 = `${args[0]},${args[1]}`;
     const point2 = `${args[2]},${args[3]}`;
     const calculatedDistance = mathHelpers.distance(parseInt(args[0]), parseInt(args[2]), parseInt(args[1]), parseInt(args[3])); // the elements inside args are strings so convert to integer
 
     console.log(`The distance between your two points: (${point1}), (${point2}) is ${calculatedDistance}`);
 
-    fs.mkdir(directory, (err) => {
+    const newDirName = checkDir(dirName);
+    console.log(newDirName);
+
+    fs.mkdir("./" + newDirName, (err) => {
         if (err) {
             console.log(err);
         } else {
-            fs.writeFile(directory + "./points.txt", "utf-8", (err, data) => {
+            fs.writeFile(newDirName + "/points.txt", String(calculatedDistance), "utf-8", (err) => {
                 if (err) {
                     console.log(err);
                 } else {
                     console.log("Content saved")
                 }
-
-                fs.appendFile(directory + "./points.txt", data, (err) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                })
             })
         }
     })
 };
 
 if (formattedArgs.length == 4) {
-    processInput(formattedArgs);
+    processInput(formattedArgs, dirName);
 } else {
     console.log("You did provided too many or not enough arguments.")
 }
