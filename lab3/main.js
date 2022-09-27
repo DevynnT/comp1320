4718ef417bbb8d52e8ae78f67bb25016adfb82ba
Devynn <personaldevynn@gmail.com>
2022-09-27 12:48:02 -0700

    Create appendToDir and format points.txt

--- lab3/main.js
+++ lab3/main.js
@@ -1,31 +1,42 @@
 const process = require('process');
 const fs = require('fs');
 const mathHelpers = require('./mathHelpers.js');
+const { dir } = require('console');
 
 const originalDirName = "dataPoints";
 const pointsPath = "/points.txt";
 const formattedArgs = process.argv.slice(2); // remove the first 2 args as they are uneeded
 
-const writeToDir = (dirName, data) => {
-    fs.writeFile(dirName + pointsPath, data, "utf-8", (err) => {
+const appendToDir = (dirName, appendData) => {
+    fs.appendFile(dirName + pointsPath, "\n" + appendData, (err) => {
         if (err) {
             console.log(err);
-        } else {
-            console.log("Content saved");
         }
     })
 };
 
-const createDir = (dirName, data) => {
+const writeToDir = (dirName, saveData, appendData) => {
+    fs.writeFile(dirName + pointsPath, saveData, "utf-8", (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Content saved");
+
+            appendToDir(dirName, appendData);
+        }
+    })
+};
+
+const createDir = (dirName, saveData, appendData) => {
     fs.mkdir(dirName, (err) => {
         if (err) {
             const newDirName = "temp_" + err.path;
 
             console.log(`Folder ${dirName} already exists. Creating a new folder for you called ${newDirName}.`)
 
-            createDir(newDirName, data);
+            createDir(newDirName, saveData, appendData);
         } else {
-            writeToDir(dirName, data);
+            writeToDir(dirName, saveData, appendData);
         }
     })
 };
@@ -34,10 +45,10 @@ const processInput = (args, dirName) => {
     const point1 = `${args[0]},${args[2]}`;
     const point2 = `${args[1]},${args[3]}`;
     const calculatedDistance = mathHelpers.distance(parseInt(args[0]), parseInt(args[2]), parseInt(args[1]), parseInt(args[3])); // the elements inside args are strings so convert to integer
+    const appendData = `The distance between your two points: (${point1}), (${point2}) is ${calculatedDistance}`;
+    const saveData = `User Input: ${args[0]} ${args[1]} ${args[2]} ${args[3]}`;
 
-    console.log(`The distance between your two points: (${point1}), (${point2}) is ${calculatedDistance}`);
-
-    createDir(dirName, String(calculatedDistance));
+    createDir(dirName, saveData, appendData);
 };
 
 if (formattedArgs.length == 4) {
