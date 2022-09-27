8cae985959f08d46edc2ccc0d56b0d238457b7f9
Devynn <personaldevynn@gmail.com>
2022-09-27 09:46:23 -0700

    Refactor creating the directory and file

--- lab3/main.js
+++ lab3/main.js
@@ -3,47 +3,41 @@ const fs = require('fs');
 const mathHelpers = require('./mathHelpers.js');
 
 const originalDirName = "dataPoints";
+const pointsPath = "/points.txt";
 const formattedArgs = process.argv.slice(2); // remove the first 2 args as they are uneeded
 
-const checkDir = (dirName) => {
-    let returnedDirName = dirName;
+const writeToDir = (dirName, data) => {
+    fs.writeFile(dirName + pointsPath, data, "utf-8", (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Content saved")
+        }
+    })
+};
 
-    if (fs.existsSync(dirName)) {
-        const newDirName = "temp_" + dirName;
+const createDir = (dirName, data) => {
+    fs.mkdir(dirName, (err) => {
+        if (err) {
+            const newDirName = "temp_" + err.path;
 
-        console.log(`Folder ${dirName} already exists. Creating a new folder for you called ${newDirName}`);
+            console.log(`Folder ${dirName} already exists. Creating a new folder for you called ${newDirName}`)
 
-        returnedDirName = checkDir(newDirName);
-
-        return returnedDirName;
-    } else {
-        return returnedDirName;
-    }
+            createDir(newDirName, data);
+        } else {
+            writeToDir(dirName, data);
+        }
+    })
 };
 
 const processInput = (args, dirName) => {
-    const point1 = `${args[0]},${args[1]}`;
-    const point2 = `${args[2]},${args[3]}`;
+    const point1 = `${args[0]},${args[2]}`;
+    const point2 = `${args[1]},${args[3]}`;
     const calculatedDistance = mathHelpers.distance(parseInt(args[0]), parseInt(args[2]), parseInt(args[1]), parseInt(args[3])); // the elements inside args are strings so convert to integer
 
     console.log(`The distance between your two points: (${point1}), (${point2}) is ${calculatedDistance}`);
 
-    const newDirName = checkDir(dirName);
-    console.log(newDirName);
-
-    fs.mkdir("./" + newDirName, (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            fs.writeFile(newDirName + "/points.txt", String(calculatedDistance), "utf-8", (err) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("Content saved")
-                }
-            })
-        }
-    })
+    createDir(dirName, String(calculatedDistance));
 };
 
 if (formattedArgs.length == 4) {
